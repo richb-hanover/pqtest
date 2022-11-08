b6190fdbe2b986f31927d458a937eb183cda4576
Rich Brown <richb.hanover@gmail.com>
2022-11-08 07:54:38 -0500

    Initial commit of repo

---
+++ README.md
@@ -0,0 +1,86 @@
+# Observations when learning PRQL Language
+
+The files in this repo are small PRQL sample files. I copy/paste the results into my DB manager to verify the queries.
+There are also remnants of experiments with `prql-query` and `PyPrql`.
+
+The following scattershot comments came from my attempt to replicate the queries of
+[my repo of property tax values in my town.](https://github.com/TaxFairness/TaxFairness/blob/main/README.md).
+(Don't worry - this is all public information,
+created from public Town documents.)
+Its README.md file has a number of queries against the **Property\_In\_Lyme.sqlite** database file.
+
+I realize that the parser and other facilities are not complete.
+However, they seem to be good enough to criticize.
+
+I must remind the reader that I don't know much at all about SQL.
+All my experiments are empirical - Does this work? How 'bout now? So don't take any of this as a deep insight.
+
+I have also not been especially attentive to reading the docs. Some of these notes may hint at documentation tweaks.
+Other questions may just come from the fact that I
+don't know the "correct name" to look up what I want.
+
+* The VSCode Extension for PRQL v0.3.2 is a terrific learning environment.
+I will note that it's easy to generate valid-looking PRQL that isn't valid SQL.
+
+* Functions and variables (as touted) are really
+helpful for making the PRQL code cleaner.
+The simplification of "SQL syntax"
+makes it feel easier to write queries, even if the PRQL parser is not terribly forgiving/informative.
+
+* Am I correct that the opening `[` must be on same line as the `select` or `derive` for a multi-line list? It would be helpful to  examples showing lists in brackets each on separate line.
+
+* Similarly, I was unsure how to set multiple 
+`ON...` expressions for a join.
+I ultimately said, "Well, maybe they just go in a list
+`[ ... ]`.
+I tried it and it worked. I don't know how you could make it more obvious - maybe another example in the JOIN documentation?
+
+* How to specify the correct join columns if different tables have different/conflicting names? In SQL, I would use `...from Table1 l join Table2 r on l.foo = r.bar`
+
+* Your example `celsius_of_fahrenheit` of really grinds my gears. Can you use the correct formula?
+
+   ```
+   func celsius_of_fahrenheit temp -> (temp - 32) * 5/9
+   ```
+   
+* Lack of `"""` at end of S-string gives an inscrutable message: `expected interpolate_string_inner_literal` And worse, here is an example showing the message on the wrong (first) line:
+
+   ```
+   func func1 d -> s"""printf("$%,d",{d}) as {d}"""
+   func func2 d -> s"""printf("$%,d",{d}) as {d}   
+   ```
+
+* Is it true that `filter` must precede `select`?
+I don't understand the semantics.
+`pq8.prql` shows an error if `filter` is at the end, but works OK if `filter` comes before the `select`. Why?
+
+* And the error in `pq8.prql` is _Unknown variable_ when it's clear that it has been used prior.
+
+* `pq14.prql` documents the [reported bug](https://github.com/prql/prql/issues/1116) for sub-expressions.
+
+* Use comments more prominently in examples.
+Hmmm... I say this because I felt the need to look up the comment syntax.
+I now realize that many lines in the examples have
+on-the-line comments - perhaps some full-line comments would be useful in examples?
+* What's the rule for using `x` in functions vs `{x}`?
+(Maybe it has to do with s-strings...)
+* In this example, there's an extra space before the `"-"` in the generated code. Is that expected?
+
+   ```
+   func pct o n -> s"""cast((100*({n}-{o})/{o}) as real)"""
+   yields
+   cast((100 *(125 -100) / 100) as real)
+   ```
+   
+* I didn't initially realize that it is possible to assign a "column heading" as a side-effect of a calculation: `heading_name = <expr>` Perhaps better documentation
+
+* Are variable names case-sensitive?
+Sometimes it seems they are, although I didn't write down an example.
+
+* Sometimes VSCode extension declared that a variable was not defined when I had clearly used it earlier. How does it know? 
+
+* I felt the urge to move a common expression out of the `SELECT...` list (so it wouldn't be in its own column, yet avoid duplicated code). AHAH! That's what `derive` is for - creating a variable that isn't part of a table. Correct?
+
+* What's the difference between `derive` and simply assigning a variable name in a `select`? Is it the ability to assign a name outside a `select`?
+
+* Leaving off a `","` at the end of a select line gives a vague error: `expected operator_mul, operator_add, operator_compare, operator_logical, or operator_coalesce`. Perhaps you could special case this message to "Missing a comma?"
---
+++ main.rs
@@ -0,0 +1,307 @@
+#![allow(unused)]
+
+mod backends;
+
+use anyhow::{anyhow, Result};
+use log::{debug, error, info, warn};
+
+use camino::Utf8Path;
+use std::collections::{HashMap, HashSet};
+use std::fmt::{self, Debug, Display};
+use std::io::prelude::*;
+use std::{fs, io};
+
+use clap::{Parser, ValueEnum};
+use prql_compiler::compile;
+
+cfg_if::cfg_if! {
+    if #[cfg(feature = "datafusion")] {
+        const DEFAULT_BACKEND : &str = "datafusion";
+    } else if #[cfg(feature = "duckdb")] {
+        const DEFAULT_BACKEND : &str = "duckdb";
+    } else {
+        const DEFAULT_BACKEND : &str = "";
+    }
+}
+
+const SUPPORTED_FILE_TYPES: [&str; 4] = ["csv", "json", "parquet", "avro"];
+
+// Some type aliases for consistency
+type FromType = Vec<String>;
+type SourcesType = Vec<(String, String)>;
+
+/// pq: query and transform data with PRQL
+#[derive(Parser, Debug)]
+struct Cli {
+    /// The file(s) to read data FROM if given
+    #[clap(short, long, value_parser, env = "PQ_FROM")]
+    from: Vec<String>,
+
+    /// The file to write TO if given, otherwise stdout
+    #[clap(short, long, value_parser, default_value = "-", env = "PQ_TO")]
+    to: String,
+
+    /// The database to connect to
+    #[clap(short, long, value_parser, env = "PQ_DATABASE")]
+    database: Option<String>,
+
+    /// The backend to use to process the query
+    #[clap(short, long, value_parser, env = "PQ_BACKEND")]
+    backend: Option<String>,
+
+    /// Only generate SQL without executing it against files
+    #[clap(long, value_parser)]
+    no_exec: bool,
+
+    /// The format to use for the output
+    #[clap(long, arg_enum, value_parser, env = "PQ_FORMAT")]
+    format: Option<OutputFormat>,
+
+    /// The Writer to use for writing the output
+    #[clap(
+        short,
+        long,
+        arg_enum,
+        value_parser,
+        default_value = "arrow",
+        env = "PQ_WRITER"
+    )]
+    writer: OutputWriter,
+
+    /// set this to pass a SQL query rather than a PRQL one
+    #[clap(long, value_parser, default_value = "false", env = "PQ_SQL")]
+    sql: bool,
+
+    /// The PRQL query to be processed if given, otherwise read from stdin
+    #[clap(value_parser, default_value = "-", env = "PQ_QUERY")]
+    query: String,
+}
+
+#[derive(Debug, Copy, Clone, PartialEq, Eq, PartialOrd, Ord, ValueEnum)]
+#[allow(non_camel_case_types)]
+pub enum OutputFormat {
+    csv,
+    json,
+    parquet,
+    table,
+}
+
+impl fmt::Display for OutputFormat {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{:?}", self)
+        // or, alternatively:
+        // fmt::Debug::fmt(self, f)
+    }
+}
+
+#[derive(Debug, Copy, Clone, ValueEnum)]
+#[allow(non_camel_case_types)]
+pub enum OutputWriter {
+    arrow,
+    backend,
+}
+
+fn main() -> Result<()> {
+    env_logger::init();
+    dotenvy::dotenv().ok();
+
+    let args = Cli::parse();
+    debug!("args = {args:?}");
+
+    // args.query
+    let mut query: String;
+    if args.query == "-" {
+        if atty::is(atty::Stream::Stdin) {
+            println!("Enter QUERY, then press Ctrl-d:");
+            println!();
+        }
+        query = String::new();
+        io::stdin().read_to_string(&mut query);
+        println!("---");
+    } else if args.query.ends_with(".prql") {
+        query = fs::read_to_string(&args.query)?;
+    } else {
+        query = String::from(&args.query);
+    }
+    query = query.trim().to_string();
+    debug!("query = {query:?}");
+
+    // args.from
+    // determine the sources
+    let sources = standardise_sources(&args.from)?;
+
+    if !args.sql {
+        // insert `from` clause in main pipeline if not given
+        if !query.to_lowercase().contains("from") && sources.len() > 0 {
+            query = format!("from {}|{query}", sources.last().unwrap().0);
+        }
+        debug!("query = {query:?}");
+    }
+
+    // args.sql
+    if !args.sql && !query.starts_with("prql ") {
+        // prepend a PRQL header to signal this is a PRQL query rather than a SQL one
+        query = format!("prql version:1 dialect:ansi\n{query}")
+    }
+    debug!("query = {query:?}");
+
+    // args.to
+    let to = args.to.to_string().trim_end_matches('/').to_string();
+    debug!("to = {to:?}");
+
+    debug!("args.format = {0:?}", &args.format);
+    let format: OutputFormat;
+    if let Some(args_format) = args.format {
+        if to == "-"
+            && atty::is(atty::Stream::Stdout)
+            && vec![OutputFormat::parquet].contains(&args_format)
+        {
+            return Err(anyhow!("Cannot print format={args_format:?} to stdout."));
+        } else if to != "-" && !to.ends_with(&args_format.to_string()) {
+            return Err(anyhow!(
+                "to={to:?} is incompatible with format={args_format:?}!"
+            ));
+        }
+        format = args_format;
+    } else {
+        // i.e. args.format.is_none()
+        if to == "-" {
+            format = OutputFormat::table;
+        } else {
+            format = match to
+                .split(".")
+                .last()
+                .ok_or(anyhow!("No extension format found in {to:?}"))?
+            {
+                "csv" => OutputFormat::csv,
+                "json" => OutputFormat::json,
+                "parquet" => OutputFormat::parquet,
+                "table" | "tbl" => OutputFormat::table,
+                fileext => return Err(anyhow!(".{fileext} files are currently not supported.")),
+            };
+        }
+        info!("inferred format = {format:?}");
+    }
+    debug!("format = {0:?}", &args.format);
+
+    let mut backend: String = String::from("");
+    let mut database: String = String::from("");
+
+    debug!("args.database = {0:?}", &args.database);
+    if let Some(args_database) = args.database {
+        backend = if args_database.starts_with("duckdb://") {
+            String::from("duckdb")
+        } else {
+            // FIXME: Replace this with connectorx when implemented
+            String::from("duckdb")
+        };
+        database = args_database.to_string();
+    } else {
+        backend = String::from(DEFAULT_BACKEND);
+    }
+    debug!("database = {database:?}");
+
+    debug!("args.backend = {0:?}", &args.backend);
+    if let Some(args_backend) = args.backend {
+        // an explicitly provided backend overrides the one we inferred
+        backend = args_backend;
+    }
+    debug!("backend = {backend:?}");
+
+    debug!("args.writer = {0:?}", &args.writer);
+
+    if args.no_exec || (database == "" && args.from.len() == 0 && !args.sql) {
+        let sql = get_sql_from_query(&query)?;
+        println!("{}", &sql);
+    } else {
+        let mut found_backend = false;
+
+        #[cfg(feature = "datafusion")]
+        if backend == "datafusion" {
+            // Create a tokio runtime to run async datafusion code
+            let rt = tokio::runtime::Builder::new_current_thread()
+                .enable_all()
+                .build()?;
+
+            rt.block_on(backends::datafusion::query(
+                &query,
+                &sources,
+                &to,
+                &database,
+                &format,
+                &args.writer,
+            ))?;
+            found_backend = true;
+        }
+        #[cfg(feature = "duckdb")]
+        if backend == "duckdb" {
+            backends::duckdb::query(&query, &sources, &to, &database, &format, &args.writer)?;
+            found_backend = true;
+        }
+        if !found_backend {
+            return Err(anyhow!(
+                "No backends found! Consider running with the -no-exec flag set."
+            ));
+        }
+    }
+
+    Ok(())
+}
+
+fn get_dest_from_to(to: &str) -> Result<Box<dyn Write>> {
+    // determine the destination
+    let mut dest: Box<dyn Write>;
+    if to == "-" {
+        dest = Box::new(std::io::stdout());
+    } else {
+        dest = Box::new(std::fs::File::create(&to)?);
+    }
+    Ok(dest)
+}
+
+fn get_sql_from_query(query: &str) -> Result<String> {
+    let sql = if query.starts_with("prql ") {
+        compile(query)?
+    } else {
+        query.to_string()
+    };
+    Ok(sql)
+}
+
+fn standardise_sources(from: &FromType) -> Result<SourcesType> {
+    debug!("from={from:?}");
+    let supported_file_types: HashSet<&str> = HashSet::from(SUPPORTED_FILE_TYPES);
+    // let mut sources : Vec<(String, String)> = Vec::<(String, String)>::new();
+    let mut sources: SourcesType = SourcesType::new();
+    for fromstr in from.iter() {
+        let mut fromparts: Vec<String> = fromstr.split("=").map(|s| s.to_string()).collect();
+        if fromparts.len() == 1 {
+            let filepath = Utf8Path::new(&fromparts[0]);
+            let fileext = filepath
+                .extension()
+                .ok_or(anyhow!("No extension in: {filepath}"))?;
+            if supported_file_types.contains(&fileext) {
+                // Dealing with a file
+                let last_component = filepath
+                    .components()
+                    .last()
+                    .ok_or(anyhow!("There was no last component of: {filepath}"))?;
+                let filename = last_component
+                    .as_str()
+                    .split(".")
+                    .next()
+                    .ok_or(anyhow!("No filename found in: {last_component}"))?;
+                let tablename = filename.replace(" ", "_");
+                fromparts = vec![tablename, fromparts[0].clone()];
+            } else {
+                // Dealing with a possible tablename with schema prefix
+                let tableparts: Vec<&str> = fromparts[0].split(" ").collect();
+                let tablename = tableparts.last().ok_or(anyhow!("No last tablepart"))?;
+                fromparts = vec![tablename.to_string(), fromparts[0].clone()];
+            }
+        }
+        sources.push((fromparts[0].clone(), fromparts[1].clone()));
+    }
+    debug!("sources={sources:?}");
+    Ok(sources)
+}
